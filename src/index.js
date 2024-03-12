ccd22560585579f61d95881605d4bb7a68fb525d
LVMdS <leo92vinicius@gmail.com>
2024-03-12 20:34:19 -0300

    Leonardo Vinicius

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-var valorA;
-valorFinal = valorA;
-console.log(valorFinal);
+var apel = "Leo"
+const nome = "Leonardo Vinicius"
+const sobre = "Martins de Souza"
+var idade = 31
+console.log(`Meu nome é ${nome} ${sobre}. Pode me chamar de ${apel}, e tenho ${idade} anos.`);
