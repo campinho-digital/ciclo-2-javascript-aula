510b7f81118e54245e4791928521cb32b4c75f8f
leonardo <ldrodrigues.jv@gmail.com>
2024-03-12 20:58:50 -0300

    enviando commit do leonardo rodrigues

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 var valorA;
 valorFinal = valorA;
 console.log(valorFinal);
+
+//Leonardo Rodrigues
+void function(){ 
+    const nome = 'Leonardo';
+    let sobrenome1 = 'Delgado';
+    let sobrenome2= 'Rodrigues';
+    let resultado = "Meu nome é "+nome +" " + sobrenome1 +" "+ sobrenome2;
+    console.log(resultado);
+    //vai dar um erro mais foi para mostrar que variável
+    //quando é constante não pode ser alterado
+    nome="João";
+}();
