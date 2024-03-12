649a0172180ce12a25ce38f1f4f511efd9f73fe5
LVMdS <leo92vinicius@gmail.com>
2024-03-12 20:55:18 -0300

    troca de var para let

--- src/index.js
+++ src/index.js
@@ -67,8 +67,8 @@ meuNome(nome)
 
 
 
-var apel = "Leo"
+let apel = "Leo"
 const nome = "Leonardo Vinicius"
 const sobre = "Martins de Souza"
-var idade = 31
+let idade = 31
 console.log(`Meu nome é ${nome} ${sobre}. Pode me chamar de ${apel}, e tenho ${idade} anos.`);
