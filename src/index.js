41a13b7c004ea21780231639f2d4ed77d10617a5
unknown <tiagopimenta.ata@gmail.com>
2024-03-12 20:33:19 -0300

    funcão Meunome

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 var valorA;
 valorFinal = valorA;
 console.log(valorFinal);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var nome = "Tiago Pimenta"
+
+var meuNome = function (nome){
+    console.log(nome)
+}
+
+meuNome(nome)
