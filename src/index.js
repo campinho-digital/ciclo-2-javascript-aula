e2b6b3b54ea82759cec2e4b68505f5494ab8cb3d
LVMdS <leo92vinicius@gmail.com>
2024-03-12 20:40:06 -0300

    Leonardo Vinicius

--- src/index.js
+++ src/index.js
@@ -36,3 +36,39 @@ var meuNome = function (nome){
 }
 
 meuNome(nome)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var apel = "Leo"
+const nome = "Leonardo Vinicius"
+const sobre = "Martins de Souza"
+var idade = 31
+console.log(`Meu nome é ${nome} ${sobre}. Pode me chamar de ${apel}, e tenho ${idade} anos.`);
